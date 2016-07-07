dae1d519c3d2c3b7a90b3a5b4c6135d924e3c786
shannpersand <heyyou@shannpersand.com>
2016-07-06 19:07:06 -0700

    adds wider spacing

--- python/05.py
+++ python/05.py
@@ -1,13 +1,17 @@
 f = CurrentFont()
 
+
+
+
 for g in f:
+    g.prepareUndo("add width")
     # print g.name, g.unicode, g.width
     # g.width = g.width + 50
     # g.rotate(15)
     # g.leftMargin = g.leftMargin + 50
     # g.rightMargin = g.rightMargin + 50
-    g.leftMargin +=50
-    g.rightMargin += 50
+    g.leftMargin +=10
+    g.rightMargin += 10
     
 #     g = CurrentGlyph()
 
@@ -16,3 +20,5 @@ for g in f:
 # print g.rightMargin
 
 # angledRightMargin (italic)
+
+g.performUndo()
