41a960ff72d65ca430f1f1dceab84cfdb1821403
shannpersand <heyyou@shannpersand.com>
2016-10-02 21:17:42 -0700

    PROOF: add   and to riter fattest

--- scripts/proofing string generation.py
+++ scripts/proofing string generation.py
@@ -1,5 +1,5 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-cap = alphabet.upper()
+alphabet = "OHRBSIMPKecognisablyfkprtz"
+# cap = alphabet.upper()
 
 
 #for letter in cap:
@@ -9,6 +9,12 @@ cap = alphabet.upper()
 #     for otherLetter in cap:
 #         print letter + otherLetter + letter + otherLetter,
 
-for letter in cap:
-    for otherLetter in cap:
-        print 'HH' + letter + otherLetter + 'HH'
+# for letter in cap:
+#     for otherLetter in cap:
+#         print 'HH' + letter + otherLetter + 'HH'
+        
+for letter in alphabet:
+    print 'nn' + letter + 'n' + letter + 'o' + letter + 'oo'
+    
+for letter in alphabet:
+    print 'HH' + letter + 'H' + letter + 'O' + letter + 'OO'
